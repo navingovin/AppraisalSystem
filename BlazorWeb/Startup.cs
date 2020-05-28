1dc21ff4fefea61ff17b27810a1524fb77c2cb25
Navin Govin <navindaze@gmail.com>
2020-05-28 12:07:47 +0530

    home page implementation

--- BlazorWeb/Startup.cs
+++ BlazorWeb/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorWeb.Data;
 
 namespace BlazorWeb
 {
@@ -28,7 +27,7 @@ namespace BlazorWeb
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            //services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
